1aa5b73669092a1874acda21e86cdc91d9d08f00
thusitha <thusitha.4t@gmail.com>
2021-06-12 17:44:51 +0530

    update

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload
         </p>
-        <Typology size="lg">AUSTAL</Typology>
+        <Typology size="lg">React App</Typology>
         <a
           className="App-link"
           href="https://reactjs.org"
