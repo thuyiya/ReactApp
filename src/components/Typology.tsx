3a9f700c3c161ba1b56e98079c3d8d997d2f8d79
thusitha <thusitha.4t@gmail.com>
2021-06-13 12:12:31 +0530

    lint config

--- src/components/Typology.tsx
+++ src/components/Typology.tsx
@@ -1,24 +1,25 @@
-import React, { ReactChildren, ReactChild } from 'react';
+/* eslint-disable max-len */
+import React, {ReactChildren, ReactChild} from 'react';
 
 enum TextElements {
-    xl ='h1',
-    lg = 'h2',
-    rg = 'h3',
-    sm = 'h4',
-    xs = 'h5',
-    xxs = 'h6',
-};
-
-const Typology: React.FC<{size: string, children: ReactChild | ReactChildren}> =({ size, children, ...props }) => {    
-    return React.createElement(
-        TextElements[size as keyof typeof TextElements] || TextElements.rg,
-        props,
-        children
-    );
+  xl = 'h1',
+  lg = 'h2',
+  rg = 'h3',
+  sm = 'h4',
+  xs = 'h5',
+  xxs = 'h6',
 }
 
+const Typology: React.FC<{size: string, children: ReactChild | ReactChildren}> = ({size, children, ...props}) => {
+  return React.createElement(
+    TextElements[size as keyof typeof TextElements] || TextElements.rg,
+    props,
+    children,
+  );
+};
+
 Typology.defaultProps = {
-    size: 'rg',
+  size: 'rg',
 };
 
 export default Typology;
